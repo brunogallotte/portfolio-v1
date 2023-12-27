f3cb20570425746e5aa8b4fa408df73ea2d65c87
Bruno Gallotte <brunogallotte1@gmail.com>
2023-12-27 11:55:35 -0300

    Feat: add responsive layout on hero

--- src/components/ListProjects/ListProjects.tsx
+++ src/components/ListProjects/ListProjects.tsx
@@ -1,38 +1,8 @@
-import { Card, CardProps } from "../Card/Card"
-
-import todoImg from '../../assets/todo.png'
-import twitterImg from '../../assets/twitterproject.png'
-import efoodImg from '../../assets/efood.png'
+import { projects } from "../../data/projectsData"
+import { Card } from "../Card/Card"
 
 import styles from './ListProjects.module.css'
 import { ArrowDownRightSquare } from "lucide-react"
-
-const dataProjects: CardProps[] = [
-    {
-        imgUrl: todoImg,
-        datePublished: "18/12/2023",
-        title: "To-do-challenge",
-        description: "Este é o primeiro desafio proposto pelo bootcamp de especialização em React.js do Ignite atualizado: uma to-do-list. É relativamente simples, mas o suficiente para praticar os principais fundamentos da biblioteca. O desafio cobre somente o layout desktop!",
-        linkVercel: "https://to-do-list-challenge-ignite.vercel.app/",
-        linkGitHub: "https://github.com/brunogallotte/to-do-list-challenge"
-    },
-    {
-        imgUrl: twitterImg,
-        datePublished: "18/12/2023",
-        title: "Twitter App",
-        description: "O desafio consistia em desenvolver uma API RESTful e integrar com uma interface front-end. Para construção da API optei pelo Django Rest Framework, e no front-end segui com React.js aliado ao Typescript.",
-        linkVercel: "https://twitter-ivory-one.vercel.app/",
-        linkGitHub: "https://github.com/brunogallotte/twitter"
-    },
-    {
-        imgUrl: efoodImg,
-        datePublished: "21/09/2023",
-        title: "E-Food",
-        description: "Projeto de E-Commerce desenvolvido com React.js, Typescript, Redux.js e Styled Components. Consumo de API utilizando Redux Tool Kit Query. Apresentado como conclusão do front-end da EBAC.",
-        linkVercel: "https://e-food-two.vercel.app/",
-        linkGitHub: "https://github.com/brunogallotte/e-food?tab=readme-ov-file"
-    }
-]
 
 export function ListProjects() {
     return(
@@ -40,7 +10,7 @@ export function ListProjects() {
             <div className={styles.wrapper}>
                 <strong className={styles.title}><ArrowDownRightSquare size={32} />Projects</strong>
                 <div className={styles.content}>
-                    {dataProjects.map(project => {
+                    {projects.map(project => {
                     return <Card 
                         imgUrl={project.imgUrl} 
                         datePublished={project.datePublished} 
---
+++ src/data/projectsData.ts
@@ -0,0 +1,32 @@
+import { CardProps } from "../components/Card/Card";
+
+import todoImg from '../assets/todo.png'
+import twitterImg from '../assets/twitterproject.png'
+import efoodImg from '../assets/efood.png'
+
+export const projects: CardProps[] = [
+    {
+        imgUrl: todoImg,
+        datePublished: "18/12/2023",
+        title: "To-do-challenge",
+        description: "Este é o primeiro desafio proposto pelo bootcamp de especialização em React.js do Ignite atualizado: uma to-do-list. É relativamente simples, mas o suficiente para praticar os principais fundamentos da biblioteca. O desafio cobre somente o layout desktop!",
+        linkVercel: "https://to-do-list-challenge-ignite.vercel.app/",
+        linkGitHub: "https://github.com/brunogallotte/to-do-list-challenge"
+    },
+    {
+        imgUrl: twitterImg,
+        datePublished: "18/12/2023",
+        title: "Twitter App",
+        description: "O desafio consistia em desenvolver uma API RESTful e integrar com uma interface front-end. Para construção da API optei pelo Django Rest Framework, e no front-end segui com React.js aliado ao Typescript.",
+        linkVercel: "https://twitter-ivory-one.vercel.app/",
+        linkGitHub: "https://github.com/brunogallotte/twitter"
+    },
+    {
+        imgUrl: efoodImg,
+        datePublished: "21/09/2023",
+        title: "E-Food",
+        description: "Projeto de E-Commerce desenvolvido com React.js, Typescript, Redux.js e Styled Components. Consumo de API utilizando Redux Tool Kit Query. Apresentado como conclusão do front-end da EBAC.",
+        linkVercel: "https://e-food-two.vercel.app/",
+        linkGitHub: "https://github.com/brunogallotte/e-food?tab=readme-ov-file"
+    }
+]
